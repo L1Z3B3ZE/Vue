c0f43aae34d900b4bb30ca4de3de126aed744209
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-01-29 13:39:52 +0700

    Перебор массива

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        onSale: true
+        details: ['80% cotton', '20% polyester', 'Gender-neutral']
     }
 })
