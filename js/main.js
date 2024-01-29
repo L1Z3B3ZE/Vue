34fb370335c544bec52bc84d0f23281cf573bb9b
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-01-29 20:50:45 +0700

    Практическая работа №7

--- js/main.js
+++ js/main.js
@@ -3,23 +3,25 @@ let app = new Vue({
     data: {
         product: "Socks",
         brand: 'Vue Mastery',
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         cart: 0,
+        onSale: false,
 
     },
     methods: {
@@ -33,7 +35,20 @@ let app = new Vue({
     computed: {
         title() {
             return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if(this.onSale){
+                return this.brand + " " + this.product + " " + 'is on sale'
+            }
+            else return this.brand + " " + this.product + " " + 'is not on sale'
         }
+
     }
 
 })
