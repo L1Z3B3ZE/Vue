f848c275ff01b04ff31120ebcd27e4812ea048f5
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-01-29 23:49:25 +0700

    Реализация добавления отзывов на товар

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
+Vue.component('product-review', {
+    template: `
+       <form class="review-form" @submit.prevent="onSubmit">
+     <p>
+       <label for="name">Name:</label>
+       <input id="name" v-model="name" placeholder="name">
+     </p>
+    
+     <p>
+       <label for="review">Review:</label>
+       <textarea id="review" v-model="review"></textarea>
+     </p>
+    
+     <p>
+       <label for="rating">Rating:</label>
+       <select id="rating" v-model.number="rating">
+         <option>5</option>
+         <option>4</option>
+         <option>3</option>
+         <option>2</option>
+         <option>1</option>
+       </select>
+     </p>
+    
+     <p>
+       <input type="submit" value="Submit"> 
+     </p>
+    
+    </form>
+
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods:{
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+        }
+    }
+
+
+})
+
+
 Vue.component('product', {
     props: {
         premium: {
@@ -26,7 +82,7 @@ Vue.component('product', {
                    :style="{ backgroundColor:variant.variantColor }"
                    @mouseover="updateProduct(index)"
            ></div>
-
+          
            <button
                    v-on:click="addToCart"
                    :disabled="!inStock"
@@ -34,8 +90,19 @@ Vue.component('product', {
            >
                Add to cart
            </button>
-           <button v-on:click="removeFromCart">Remove from cart</button>
-       
+       </div>
+       <div>
+           <h2>Reviews</h2>
+           <p v-if="!reviews.length">There are no reviews yet.</p>
+           <ul>
+               <li v-for="review in reviews">
+               <p>{{ review.name }}</p>
+               <p>Rating: {{ review.rating }}</p>
+               <p>{{ review.review }}</p>
+               </li>
+           </ul>
+       </div> 
+       <product-review @review-submitted="addReview"></product-review>
        </div>
    </div>
  `,
@@ -57,22 +124,24 @@ Vue.component('product', {
                     variantId: 2235,
                     variantColor: 'blue',
                     variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                    variantQuantity: 13210
+                    variantQuantity: 0
                 }
             ],
+            reviews: []
         }
     },
     methods: {
         addToCart() {
             this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
-        removeFromCart(){
-            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
-        },
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
+
     },
     computed: {
         title() {
@@ -92,6 +161,8 @@ Vue.component('product', {
             }
         }
     }
+
+
 })
 let app = new Vue({
     el: '#app',
@@ -102,12 +173,6 @@ let app = new Vue({
     methods: {
         updateCart(id) {
             this.cart.push(id);
-        },
-        remFromCart(id) {
-            this.cart.pop(id);
         }
     }
 })
-
-
-
